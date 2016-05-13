d2b07a219b0305fdf63d5296a178e7f94f614f8e
Pontus Falk <Pontus Falk>
2016-05-13 09:36:06 +0200

    initial

---
+++ src/main/java/se/mirado/pontus/irc/PfIrcApplication.java
@@ -0,0 +1,12 @@
+package se.mirado.pontus.irc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfIrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfIrcApplication.class, args);
+	}
+}
